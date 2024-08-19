bc915c95e85f8536460cbfc0172d56b9e52befd6
Steppasyder <musialaj706@gmail.com>
2024-08-19 22:51:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '254110421003' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || ' VAJIRA-MD=mRsADLiT#OQSBeYWob0h9WDxolu5T9ZDvO6T1lrySHzD06ROQkIA' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=uVUAzJjS#JeMuInfRIq-DEpgt72rjGQFWpGkZci3Ahzu2kgcZpAg' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "254110421003" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
